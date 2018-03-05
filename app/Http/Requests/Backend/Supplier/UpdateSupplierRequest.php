a8642507a852ee550517ea1e882af4ddd47163b8
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-03-05 19:13:29 +0800

    Changed user()->isAdmin() to user()->can('update supplier')

--- app/Http/Requests/Backend/Supplier/UpdateSupplierRequest.php
+++ app/Http/Requests/Backend/Supplier/UpdateSupplierRequest.php
@@ -13,7 +13,7 @@ class UpdateSupplierRequest extends FormRequest
      */
     public function authorize()
     {
-        return $this->user()->isAdmin();
+        return $this->user()->can('edit supplier');
     }
 
     /**
