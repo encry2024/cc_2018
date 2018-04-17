378248dfe7c74743d49d4fdd015308dd5ecedb38
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-18 02:15:15 +0800

    Added Force Delete Supplier Request

---
+++ app/Http/Requests/Backend/Supplier/ForceDeleteSupplierRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Backend\Supplier;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ForceDeleteSupplierRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('force delete supplier');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
