96021f905263b621980ae0f04a7e2def48127382
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-18 02:26:13 +0800

    Separated the modal for maintainability.

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -38,7 +38,6 @@
 
         <div class="app-body">
             @include('backend.includes.sidebar')
-
             <main class="main">
                 @include('includes.partials.logged-in-as')
                 {!! Breadcrumbs::render() !!}
@@ -48,89 +47,11 @@
                         <div class="content-header">
                             @yield('page-header')
                         </div><!--content-header-->
-
                         @include('includes.partials.messages')
                         @yield('content')
                     </div><!--animated-->
 
-                    <div class="modal fade" tabindex="-1" role="dialog" id="order-item-modal">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">ITEM ORDER</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-
-                                <div class="modal-body">
-                                    <form class="form-horizontal" action="" method="POST">
-                                        {{ csrf_field() }}
-                                        <div class="col-lg-12">
-                                            <div class="row">
-                                                <div class="col-lg-6" style="border-right: 1px dashed #DDD;">
-                                                    <div class="form-group row">
-                                                        <label class="col-md-4 form-control-label">ITEM NAME:</label>
-
-                                                        <div class="col-sm-5">
-                                                            <label id="item-name" class="form-control-label"></label>
-                                                        </div><!--col-->
-                                                    </div><!--form-group-->
-
-                                                    <div class="form-group row">
-                                                        <label class="col-md-4 form-control-label">STOCKS:</label>
-
-                                                        <div class="col-sm-5">
-                                                            <label id="item-stocks" class="form-control-label"></label>
-                                                        </div>
-                                                    </div><!--form-group-->
-
-                                                    <div class="form-group row">
-                                                        <label class="col-md-4 form-control-label">BUYING PRICE:</label>
-
-                                                        <div class="col-sm-5">
-                                                            <label id="item-buying-price" class="form-control-label"></label>
-                                                        </div>
-                                                    </div><!--form-group-->
-
-                                                    <div class="form-group row">
-                                                        <label class="col-md-4 form-control-label">SUPPLIER:</label>
-
-                                                        <div class="col-sm-8">
-                                                            <label id="item-supplier" class="form-control-label"></label>
-                                                        </div>
-                                                    </div><!--form-group-->
-                                                </div><!--col-->
-
-                                                <div class="col-lg-6">
-                                                    <div class="form-group row">
-                                                        <label class="col-md-5 form-control-label">QUANTITY:</label>
-
-                                                        <div class="col-sm-7">
-                                                            <input type="text" class="form-control input-sm">
-                                                        </div>
-                                                    </div><!--form-group-->
-
-                                                    <div class="form-group row">
-                                                        <label class="col-md-5 form-control-label">DELIVERY DATE:</label>
-
-                                                        <div class="col-sm-7">
-                                                            <input type="date" class="form-control input-sm" forma>
-                                                        </div>
-                                                    </div><!--form-group-->
-                                                </div><!--col-->
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-sm btn-dark">ADD TO TRAY</button>
-                                    <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">CLOSE</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @include('backend.includes.modals')
                 </div><!--container-fluid-->
             </main><!--main-->
 
