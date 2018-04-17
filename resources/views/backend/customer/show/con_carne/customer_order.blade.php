fb086305d872b1730e1927cc1ea5fd960c4d108d
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-18 02:24:57 +0800

    Separated the customer_order table for maintainability.

---
+++ resources/views/backend/customer/show/con_carne/customer_order.blade.php
@@ -0,0 +1,22 @@
+<h5 class="card-title">
+    CUSTOMER ORDERS
+    <button class="btn btn-danger pull-right btn-sm" title="Remove Item" data-toggle="tooltip" id="remove_btn"><i class="fa fa-trash"></i></button>
+</h5>
+<hr>
+<table class="table table-sm table-striped order-table">
+    <thead>
+        <th style="width: 20%;">Item</th>
+        <th style="width: 15%;">Price</th>
+        <th style="width: 13%;">Quantity</th>
+        <th style="width: 15%;">Cost</th>
+        <th style="width: 17%;">Delivery Date</th>
+    </thead>
+    <tfoot>
+        <tr>
+            <td>TOTAL ITEMS: <span id="item_count">0</span></td>
+            <td colspan="4">TOTAL COST: <span id="total_cost_label">PHP 0.00</span></td>
+        </tr>
+    </tfoot style="border-top: 1px solid #ddd;">
+    <tbody id="customer_order_container">
+    </tbody>
+</table>
