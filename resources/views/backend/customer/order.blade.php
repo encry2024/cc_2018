2635fb3cb127dac7a87881594608516390efd1d7
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-18 02:23:27 +0800

    Added some col, and row. Removed scripts.

--- resources/views/backend/customer/order.blade.php
+++ resources/views/backend/customer/order.blade.php
@@ -7,49 +7,55 @@
 @endsection
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h4 class="card-title mb-0">
-                        {{ __('labels.backend.customers.management') }}
-                        <small class="text-muted">{{ __('labels.backend.customers.order') }}</small>
-                    </h4>
-                </div><!--col-->
-            </div><!--row-->
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h4 class="card-title mb-0">
+                                {{ __('labels.backend.customers.management') }}
+                                <small class="text-muted">{{ __('labels.backend.customers.order') }}</small>
+                            </h4>
+                        </div><!--col-->
+                    </div><!--row-->
+                </div><!--card-body-->
+            </div><!--card-->
+        </div><!--col-->
+    </div><!--row-->
 
-            <hr>
+    <div class="row">
+        <div class="col">
+            <div id="ajaxSpinnerContainer">
+                <div id="ajaxSpinner"></div>
+            </div>
+            <div class="card-group">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row mt-4 mb-4">
+                            <div class="col">
+                                @include('backend.customer.show.con_carne.order_menu')
+                            </div><!--col-->
+                        </div><!--row-->
+                    </div><!--card-body-->
+                </div><!--card-->
 
-            <div class="row mt-4 mb-4">
-                <div class="col">
-                    @include('backend.customer.show.con_carne.inventory')
-                </div><!--col-->
-            </div><!--row-->
-        </div><!--card-body-->
-    </div><!--card-->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row mt-4 mb-4">
+                            <div class="col">
+                                @include('backend.customer.show.con_carne.customer_order')
+                            </div><!--col-->
+                        </div><!--row-->
+                    </div><!--card-body-->
+                </div><!--card-->
+            </div><!--card-group-->
+        </div><!--col-->
+    </div><!--row-->
 
-    <script>
-        // Show Item
-        function restockFunction(showItem)
-        {
-            window.location.href = showItem;
-        }
-
-        // Order Item
-        function orderItem(itemId, itemName, itemStock, itemSupplier, itemBuyingPrice)
-        {
-            $("#order-item-modal").modal('show');
-
-            document.getElementById("item-name").innerHTML = itemName;
-            document.getElementById("item-stocks").innerHTML = itemStock;
-            document.getElementById("item-supplier").innerHTML = itemSupplier;
-            document.getElementById("item-buying-price").innerHTML = itemBuyingPrice;
-        }
-
-        $(document).ready(function() {
-            $("#add-to-tray").click(function() {
-
-            });
-        })
-    </script>
+    <br>
 @endsection
+
+@push('after-scripts')
+    @include('backend.customer.scripts.ordering_script')
+@endpush
