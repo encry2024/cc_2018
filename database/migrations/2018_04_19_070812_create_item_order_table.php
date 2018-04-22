55a5a963504c5c937df8baf50a87e37f7c6219a8
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-23 01:44:53 +0800

    Added Item Order Migration.

---
+++ database/migrations/2018_04_19_070812_create_item_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id')->unsigned();
+            $table->integer('item_id')->unsigned();
+            $table->decimal('requested_quantity', 18, 2);
+            $table->decimal('total_cost', 18, 2);
+            $table->date('delivery_date');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_order');
+    }
+}
