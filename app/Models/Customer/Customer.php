63172178d7ac0c45d8620016e7f6b69ea6f41ade
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-23 01:36:14 +0800

    Added Customer Relationship, and Soft Delete traits. And also attributes.

--- app/Models/Customer/Customer.php
+++ app/Models/Customer/Customer.php
@@ -5,12 +5,18 @@ namespace App\Models\Customer;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Customer\Traits\Attribute\CustomerAttribute;
+use App\Models\Customer\Traits\Relationship\CustomerRelationship;
 
 class Customer extends Model
 {
-    use CustomerAttribute, SoftDeletes;
+    // Traits
+    use SoftDeletes,
+        CustomerAttribute,
+        CustomerRelationship;
 
     protected $fillable = ['name', 'email', 'contact_number', 'address', 'discount', 'credit_limit'];
 
     protected $dates = ['deleted_at'];
+
+    protected $appends = ['current_credit', 'progress_credit'];
 }
