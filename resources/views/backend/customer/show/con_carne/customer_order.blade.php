9afb843e36366432ef4dddffa01ef2c8206f7d96
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-23 01:49:15 +0800

    Added Total Cost on table.

--- resources/views/backend/customer/show/con_carne/customer_order.blade.php
+++ resources/views/backend/customer/show/con_carne/customer_order.blade.php
@@ -6,7 +6,7 @@
 <table class="table table-sm table-striped order-table">
     <thead>
         <th style="width: 20%;">Item</th>
-        <th style="width: 15%;">Price</th>
+        <th style="width: 15%;">Price/kg</th>
         <th style="width: 13%;">Quantity</th>
         <th style="width: 15%;">Cost</th>
         <th style="width: 17%;">Delivery Date</th>
@@ -14,7 +14,9 @@
     <tfoot>
         <tr>
             <td>TOTAL ITEMS: <span id="item_count">0</span></td>
-            <td colspan="4">TOTAL COST: <span id="total_cost_label">PHP 0.00</span></td>
+            <td></td>
+            <td>TOTAL COST:</td>
+            <td colspan="2">PHP <span id="total_cost_label">0.00</span></td>
         </tr>
     </tfoot style="border-top: 1px solid #ddd;">
     <tbody id="customer_order_container">
