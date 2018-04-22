197cd2ffdc22d422a3b7a28aa9a193c686aa6a8b
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-23 01:42:01 +0800

    Added Traits: SoftDeletes, OrderRelationship, OrderAttribute. Added fillable, and Appends.

--- app/Models/Order/Order.php
+++ app/Models/Order/Order.php
@@ -3,8 +3,24 @@
 namespace App\Models\Order;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Order\Traits\Relationship\OrderRelationship;
+use App\Models\Order\Traits\Attribute\OrderAttribute;
 
 class Order extends Model
 {
+    use SoftDeletes,
+        OrderRelationship,
+        OrderAttribute;
     //
+    protected $fillable = [
+        'user_id',
+        'status',
+        'balance',
+        'collection_date',
+        'payment_type',
+        'note'
+    ];
+
+    protected $appends = ['payment_method', 'due_date', 'remaining_balance'];
 }
