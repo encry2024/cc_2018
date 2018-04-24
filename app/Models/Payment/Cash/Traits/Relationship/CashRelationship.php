008670fd1d880c550c8cd8919caa9364e38123de
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-24 14:15:31 +0800

    Added User Relationship.

--- app/Models/Payment/Cash/Traits/Relationship/CashRelationship.php
+++ app/Models/Payment/Cash/Traits/Relationship/CashRelationship.php
@@ -3,11 +3,17 @@
 namespace App\Models\Payment\Cash\Traits\Relationship;
 
 use App\Models\Payment\Payment\Payment;
+use App\Models\Auth\User;
 
 trait CashRelationship
 {
     public function payment()
     {
         return $this->morphMany(Payment::class, 'paymentable');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
