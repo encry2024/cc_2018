f70b6ee25876a3e2c4ed1f01dc04b0381716442e
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-24 14:16:08 +0800

    Added User Relationship.

--- app/Models/Payment/Check/Traits/Relationship/CheckRelationship.php
+++ app/Models/Payment/Check/Traits/Relationship/CheckRelationship.php
@@ -3,11 +3,17 @@
 namespace App\Models\Payment\Check\Traits\Relationship;
 
 use App\Models\Payment\Payment\Payment;
+use App\Models\Auth\User;
 
 trait CheckRelationship
 {
     public function payment()
     {
         return $this->morphMany(Payment::class, 'paymentable');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
