bd879255abe3b071acd2aa3b61eb581a4333bc2e
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-24 14:14:02 +0800

    Added Delete Button.

--- app/Models/Cart/Traits/Attribute/CartAttribute.php
+++ app/Models/Cart/Traits/Attribute/CartAttribute.php
@@ -13,17 +13,17 @@ trait CartAttribute
     public function getUpdateButtonAttribute()
     {
         if ($this->status == "QUEUE")
-            return 
+            return
                 '<a href="'.route('admin.cart.update', $this).'"
-                    class="btn btn-info btn-xs" 
+                    class="btn btn-info btn-xs text-white"
                     data-method="patch"
                     data-trans-button-cancel="Cancel"
                     data-trans-button-confirm="Request"
                     data-trans-title="Request a restock for this item?">
-                    <i class="fa fa-code-fork" data-toggle="tooltip" data-placement="top" title="Request"></i>
+                    <i class="fa fa-refresh" data-toggle="tooltip" data-placement="top" title="Request"></i>
                 </a>';
         elseif ($this->status == "REQUESTED") {
-            return 
+            return
             '<a href="'.route('admin.cart.update', $this).'"
                 class="btn btn-success btn-xs"
                 data-method="patch"
@@ -40,9 +40,29 @@ trait CartAttribute
     /**
      * @return string
      */
+    public function getDeleteButtonAttribute()
+    {
+        if ($this->status == "QUEUE") {
+            return
+                '<a href="'.route('admin.cart.destroy', $this).'"
+                    class="btn btn-danger btn-xs text-white"
+                    data-method="delete"
+                    data-trans-button-cancel="No"
+                    data-trans-button-confirm="Yes"
+                    data-trans-title="Are you sure you want to cancel this request?">
+                    <i class="fa fa-trash" data-toggle="tooltip" data-placement="top" title="Cancel Request"></i>
+                </a>';
+        }
+    }
+
+    /**
+     * @return string
+     */
     public function getActionButtonsAttribute()
     {
         return '<div class="btn-group btn-group-sm" role="group" aria-label="Cart Actions">
-            '.$this->update_button.'</div>';
+            '.$this->update_button.'
+            '.$this->delete_button.'
+            </div>';
     }
 }
