1386efab462f8d5c907f6f38ff583d6a4e4c3d3a
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-30 21:34:11 +0800

    Added Scope, and Attribute.

--- app/Models/Cashflow/Cashflow.php
+++ app/Models/Cashflow/Cashflow.php
@@ -4,15 +4,21 @@ namespace App\Models\Cashflow;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Cashflow\Traits\Relationship\CashflowRelationship;
+use App\Models\Cashflow\Traits\Attribute\CashflowAttribute;
+use App\Models\Cashflow\Traits\Scope\CashflowScope;
 
 class Cashflow extends Model
 {
     //
-    use CashflowRelationship;
+    use CashflowScope,
+        CashflowAttribute,
+        CashflowRelationship;
 
     protected $fillable = [
         'amount',
         'cashflowable_id',
         'cashflowable_type'
     ];
+
+    protected $append = ['type'];
 }
