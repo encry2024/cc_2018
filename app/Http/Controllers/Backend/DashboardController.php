fd44234815014911631b749339518cf9a71c0a10
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-04-30 21:33:59 +0800

    Added Cashflow Views.

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Payment\Check\Check;
 use App\Models\Order\Order;
+use App\Models\Cashflow\Cashflow;
 
 /**
  * Class DashboardController.
@@ -16,9 +17,23 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $checks     = Check::where('type', 'post-dated')->where('status', 'PENDING')->get();
-        $pending_orders   = Order::where('status', 'PENDING')->get();
+        $checks             = Check::where('type', 'post-dated')->where('status', 'PENDING')->get();
+        $pending_orders     = Order::where('status', 'PENDING')->get();
+        $cashflows          = Cashflow::whereDate('created_at', date('Y-m-d'))->get();
+        $total_payables     = Cashflow::totalPayables();
+        $total_receivables  = Cashflow::totalReceivables();
+        $graph_receivables  = Cashflow::receivables();
+        $graph_orders       = Cashflow::orders();
+        $graph_expenses     = Cashflow::expenses();
 
-        return view('backend.dashboard')->withChecks($checks)->with('pending_orders', $pending_orders);
+        return view('backend.dashboard')
+            ->withChecks($checks)
+            ->with('pending_orders', $pending_orders)
+            ->withCashflows($cashflows)
+            ->withReceivables($total_receivables)
+            ->withPayables($total_payables)
+            ->with('graph_receivables', $graph_receivables)
+            ->with('graph_orders', $graph_orders)
+            ->with('graph_expenses', $graph_expenses);
     }
 }
